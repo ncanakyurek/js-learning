4541537d6f5df4ecb79ad03b9795cdd6fff9ae2d
Naki'nin Mac'i <nakicanakyurek@Nakinin-Mac-mini.local>
2026-05-22 22:40:50 +0300

    Added basic condition examples

---
+++ conditions.js
@@ -0,0 +1,15 @@
+const age = 25;
+
+
+if (age >= 18) {
+console.log (`You are ${age} years old. You are an adult.`);
+} else {
+
+console.log (`You are ${age} years old. You are not an adult.`);
+
+}
+
+
+
+
+
