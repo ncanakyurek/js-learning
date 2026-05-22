f89ecf1d7e24f99874eded85b23e03e1a839f7a5
Naki'nin Mac'i <nakicanakyurek@Nakinin-Mac-mini.local>
2026-05-22 15:41:54 +0300

    Create Objects

---
+++ objects.js
@@ -0,0 +1,22 @@
+const users = {
+
+name: 'John Doe',
+
+age: 30,
+
+email: 'john@doe.com',
+
+job: 'Software Engineer'
+
+}; //  create object users named with 4 properties name, age, email, job
+
+
+console.log(users); // print the users object to the console
+users.birthday = '1990-01-01';  // news property add
+console.log(users);
+users.job = 'Senior Software Engineer'; // update the value of the job property
+delete users.email; // delete the email property from the users object
+console.log(users); 
+
+
+
