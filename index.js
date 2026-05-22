d884c32573679cd6a59495522f0df8592bb5f398
Naki'nin Mac'i <nakicanakyurek@Nakinin-Mac-mini.local>
2026-05-22 15:26:41 +0300

    Added object property examples

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const users=[
+const users = {
 
-    "John Doe",
-    "Jane Doe",
-    "Alice Smith",
-    "Bob Johnson",
-    "Charlie Brown",
-    "Emily Davis",
-    "Michael Wilson",
-    "Sarah Miller",
-    "David Anderson",
-    "Jessica Taylor"
-];
+name: 'John Doe',
 
- const userList=document.getElementById("users-list");
- users.map((user) => {
-   
-    userList.innerHTML += `<li>${user}</li>`;
- });    
+age: 30,
+
+email: 'john@doe.com',
+
+job: 'Software Engineer'
+
+}; //  create object users named with 4 properties name, age, email, job
+
+
+console.log(users); // print the users object to the console
+users.birthday = '1990-01-01';  // news property add
+console.log(users);
+users.job = 'Senior Software Engineer'; // update the value of the job property
+delete users.email; // delete the email property from the users object
+console.log(users); 
+
+
+
